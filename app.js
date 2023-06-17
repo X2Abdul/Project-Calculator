7429b17566f4f8b3b9b86f18c2795b5d18d8b814
X2Abdul <87334764+X2Abdul@users.noreply.github.com>
2023-06-17 05:39:53 +0100

    basic calculations - complete

--- app.js
+++ app.js
@@ -6,18 +6,8 @@ let output = '';
 
 
 
-const multiply = function(numbers) {
-  if(!Array.isArray(numbers) || numbers.length ===0){
-    return 0;
-  }
-  let ans = 1;
-  for(let i = 0; i < numbers.length;i++){
-    if(typeof numbers[i] === 'number'){
-      ans *= numbers[i];
-    }
-  }
-  
-  return ans;
+const multiply = function(num1, num2) {
+  return num1 * num2;
 };
 
 const add = function(num1, num2) {
@@ -36,9 +26,47 @@ const modulus = function(num1, num2){
   return num1 % num2;
 }
 
+
+const operate = function(operator, num1, num2){
+  switch(operator){
+    case '−':
+      return subtract(num1, num2);
+
+    case '+':
+      return add(num1, num2);
+
+    case '×':
+      return multiply(num1, num2);
+
+    case '÷':
+      return divide(num1, num2);
+
+    case '%':
+      return modulus(num1, num2);
+
+  }
+}
+
 const evaluate = function(output){
+  if (output.includes('+') || output.includes('−') || output.includes('×') || output.includes('÷') || output.includes('%')) {
+    let operatorIndex = 0;
+    for (let i = 0; i < output.length; i++) {
+      if (isNaN(parseInt(output[i]))) {
+        operatorIndex = i;
+        break;
+      }
+    }
+    let num1 = parseInt(output.substring(0, operatorIndex));
+    let num2 = parseInt(output.substring(operatorIndex + 1));
+    let op = output[operatorIndex];
+    console.log(op, num1, num2);
+
+    return operate(op, num1, num2);
+    
+  }
   
 };
+
 
 buttons.forEach(button => {
     button.addEventListener('click', function(){
@@ -52,9 +80,9 @@ buttons.forEach(button => {
           output = output.slice(0, -1);
           screenText.textContent = output;
         }else if(button.id === '='){
-          
+          output = evaluate(output); 
+          screenText.textContent = output;
         }else{
-          console.log(button.textContent);
           output += button.textContent;
           screenText.textContent = output;
         }
