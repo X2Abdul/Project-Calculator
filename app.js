dcd9cc4a0731ba8c46bf7a9cd4af7b7eb30874e7
X2Abdul <87334764+X2Abdul@users.noreply.github.com>
2023-06-19 00:33:14 +0100

    multiple calculation and calculation with multiple operators - complete

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const screenText = document.querySelector('.screenText');
 let output = '';
 
 
-
-
 const multiply = function(num1, num2) {
   return num1 * num2;
 };
@@ -47,47 +45,91 @@ const operate = function(operator, num1, num2){
   }
 }
 
+let Answer = 0;
+
 const evaluate = function(output){
   if (output.includes('+') || output.includes('−') || output.includes('×') || output.includes('÷') || output.includes('%')) {
-    let operatorIndex = 0;
-    for (let i = 0; i < output.length; i++) {
-      if (isNaN(parseInt(output[i]))) {
-        operatorIndex = i;
-        break;
+    const operatorStack = [];
+    const operandStack = [];
+
+    const numList = ['0','1','2','3','4','5','6','7','8','9'];
+    const operatorList = ['+','−', '×', '÷', '%'];
+    let number = '';
+    for(let i = 0; i < output.length; i++){
+      if(numList.includes(output[i])){
+        number += output[i];
+      } else if(operatorList.includes(output[i])){
+        operandStack.push(number);
+        operatorStack.push(output[i]);
+        number = '';
+      }else if(i < output.length){
+        operandStack.push(number);
+      }
+
+      if (i === output.length - 1 && number !== '') {
+        operandStack.push(number);
       }
     }
-    let num1 = parseInt(output.substring(0, operatorIndex));
-    let num2 = parseInt(output.substring(operatorIndex + 1));
-    let op = output[operatorIndex];
-    console.log(op, num1, num2);
-
-    return operate(op, num1, num2);
+    operandStack.reverse();
+    operatorStack.reverse();
     
+    let operand1 = '';
+    let operand2 = '';
+    let operator = '';
+    let finalAns = 0;
+    while(operandStack.length !== 0){
+      if(finalAns === 0){
+        operand1 = operandStack.pop();
+        operand2 = operandStack.pop();
+        operator = operatorStack.pop();
+      }else{
+        operand1 = finalAns.toString();
+        operand2 = operandStack.pop();
+        operator = operatorStack.pop();
+      }
+      
+      finalAns = operate(operator, parseInt(operand1), parseInt(operand2));
+      
+      console.log(operand1 + operator + operand2 + " = " + operate(operator, parseInt(operand1), parseInt(operand2)));
+    }
+    Answer = finalAns;
+    return finalAns;
   }
-  
 };
 
 
+let evaluated = false;
+
 buttons.forEach(button => {
-    button.addEventListener('click', function(){
-      if(output.length < 10 || button.classList.contains('remove') || button.id === '='){
-        var audio = new Audio('./SoundEffects/clickSound.wav');
-        audio.play();
-        if(button.id === 'AC'){
-          output = '';
-          screenText.textContent = output;
-        }else if(button.id === 'C'){
-          output = output.slice(0, -1);
-          screenText.textContent = output;
-        }else if(button.id === '='){
-          output = evaluate(output); 
-          screenText.textContent = output;
-        }else{
+  button.addEventListener('click', function(){
+    if (output.length < 10 || button.classList.contains('remove') || button.id === '=') {
+      var audio = new Audio('./SoundEffects/clickSound.wav');
+      audio.play();
+      if (button.id === 'AC') {
+        output = '';
+        screenText.textContent = output;
+        evaluated = false;
+      } else if (button.id === 'C') {
+        output = output.slice(0, -1);
+        screenText.textContent = output;
+        evaluated = false;
+      } else if (button.id === '=') {
+        let result = evaluate(output);
+        output = result.toString();
+        screenText.textContent = output;
+        evaluated = true;
+      } else {
+        if (evaluated) {
+          output = Answer;
           output += button.textContent;
-          screenText.textContent = output;
+          evaluated = false;
+        } else {
+          output += button.textContent;
         }
+        screenText.textContent = output;
+      }
     }
-    });
+  });
 });
 
   
